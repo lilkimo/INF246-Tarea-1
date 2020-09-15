0c61e8ca445a3088a9c46ddd9f1dd3c15c4c67c8
Crastiv <camilo.gonzalezn@sansano.usm.cl>
2020-09-15 19:01:04 -0300

    create a folder named quetal

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <sys/syscall.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 int main() {
     printf("Hello, World!");
+    int ret;
+    ret = mkdir("quetal",S_IRUSR);
     return 0;
 }
