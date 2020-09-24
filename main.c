26f8955b576dc1494a0c2614114fbf9a79d393bc
Crastiv <camilo.gonzalezn@sansano.usm.cl>
2020-09-24 17:30:17 -0300

    crear carpeta y copiar archivo

--- main.c
+++ main.c
@@ -91,6 +91,33 @@ juego * obtenerJuegos(int *largo) {
     *largo = indice;
     return juegos;
 }
+void crear_carpeta(char *carpeta){
+    mkdir(carpeta,S_IRUSR); // El segundo parametro son los permisos, el que coloque es temporal
+    return;
+}
+
+void copiar_archivo(char *destino, char *origen) {
+   char ch;
+   FILE *source, *target;
+   source = fopen(origen, "r");
+   if( source == NULL ) {
+      printf("Error al abrir el archivo\n");
+      exit(EXIT_FAILURE);
+   }
+   target = fopen(destino, "w");
+   if( target == NULL ) {
+      fclose(source);
+      printf("Error al crear el archivo\n");
+      exit(EXIT_FAILURE);
+   }
+   while( ( ch = fgetc(source) ) != EOF )
+      fputc(ch, target);
+   fclose(source);
+   fclose(target);
+   return;
+}
+
+
 
 
 int main() {
